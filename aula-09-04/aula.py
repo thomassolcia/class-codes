13d865ca41792b63f3f0c606b99b7032baf49ae1
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-04-10 05:30:44 -0300

    Divisão em 4 limiares

--- aula-09-04/aula.py
+++ aula-09-04/aula.py
@@ -4,41 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 
-
 st.title('Aula do dia 09/04')
-
-
 image = Image.open('monte_fuji.jpg')
-
 imagem_color_arr = np.array(image)
-
 img_gray = np.mean(imagem_color_arr, axis=2)
-
 st.text(img_gray.shape)
 
-limiar = st.slider('Limiar?', 0, 255, 25)
-
-st.text(limiar)
+limiar64 = st.slider('Limiar 1?', 0, 64, 0)
+limiar128 = st.slider('Limiar 2?', 64, 128, 64)
+limiar192 = st.slider('Limiar 3?', 128, 192, 128)
+limiar255 = st.slider('Limiar 4?', 192, 255, 192)
+st.text(limiar64)
 
 img_gray = np.mean(imagem_color_arr, axis=2)
 
-# img_gray[img_gray != limiar] = 255
-img_gray[0 < img_gray and img_gray < 64]  = 0
-img_gray[64 < img_gray and img_gray < 128]  = 64
-img_gray[128 < img_gray and img_gray < 192]  = 128
-img_gray[img_gray > 192]  = 192
-# img_gray[img_gray < limiar] = 191
-# img_gray[img_gray > limiar] = 127
-# img_gray[img_gray > limiar] = 64
-# img_gray[img_gray < limiar] = 0
+img_gray[img_gray < limiar64] = 0
+img_gray[(img_gray < limiar128).all() and (img_gray > limiar64).all()] = 64
+img_gray[(img_gray < limiar192).all() and (img_gray > limiar128).all()] = 128
+img_gray[img_gray > limiar192] = 192
 
 new_image = Image.fromarray(img_gray)
-
 plt.axis('off')
 plt.imshow(new_image)
 plt.show()
 st.pyplot()
-
-# new_image = Image.fromarray(imagem_color_arr)
-
-# st.image(new_image, caption='Sunrise by the mountains', use_column_width=True)
