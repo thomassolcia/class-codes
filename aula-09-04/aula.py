5a5c8f24b6e50c772adc231d738d3355f78a59b4
Thomas Solcia do Nascimento <thomas.nascimento@edu.unifil.br>
2020-04-17 17:34:37 -0300

    Atividade corrigida

--- aula-09-04/aula.py
+++ aula-09-04/aula.py
@@ -13,30 +13,38 @@ image = Image.open('unsplash.jpg')
 imagem_color_arr = np.array(image)
 img_gray = np.mean(imagem_color_arr, axis=2)
 st.text(img_gray.shape)
-img_gray = np.mean(imagem_color_arr, axis=2)
 
-ax = int(255/num_color)
-for x in range(0,num_color):
-    if(x == 0):
-        img_gray[img_gray > 255-ax]  = 255
-    else:
-        img_gray[(((ax*x)+1) < img_gray) & (img_gray <(ax*(x*2))-ax)] = (ax*x)+1
+if(num_color == 2):
+    img_gray[img_gray < 127]  = 0
+    img_gray[img_gray >= 127]  = 255
+else:
+    ax = int(255/num_color)
+    for x in range(0,num_color):
+        if(x == 0):
+            img_gray[img_gray < ((ax+1)*1)] = 0
+            img_gray[img_gray > (255-ax)]  = 255
+        else:
+            img_gray[(((ax+1)*x) < img_gray) & (img_gray <((ax+1)*(x+1)))] = (ax+1)*x
 
 new_image = Image.fromarray(img_gray)
-st.image([new_image.convert("L"), image], caption=['Cinza', 'colorida'], width=480,) 
+st.image([new_image.convert("L"), image], caption=['Cinza', 'colorida'], width=480,)
 
 image1 = Image.open('monte_fuji.jpg')
 imagem_color_arr1 = np.array(image1)
 img_gray1 = np.mean(imagem_color_arr1, axis=2)
 st.text(img_gray1.shape)
-img_gray1 = np.mean(imagem_color_arr1, axis=2)
 
-ax = int(255/num_color)
-for x in range(0,num_color):
-    if(x == 0):
-        img_gray1[img_gray1 > 255-ax]  = 255
-    else:
-        img_gray1[(((ax*x)+1) < img_gray1) & (img_gray1 <(ax*(x*2))-ax)] = (ax*x)+1
+if(num_color == 2):
+    img_gray1[img_gray1 < 127]  = 0
+    img_gray1[img_gray1 >= 127]  = 255
+else:
+    ax = int(255/num_color)
+    for x in range(0,num_color):
+        if(x == 0):
+            img_gray1[img_gray1 < ((ax+1)*1)] = 0
+            img_gray1[img_gray1 > (255-ax)]  = 255
+        else:
+            img_gray1[(((ax+1)*x) < img_gray1) & (img_gray1 <((ax+1)*(x+1)))] = (ax+1)*x
 
 new_image1 = Image.fromarray(img_gray1)
-st.image([new_image1.convert("L"), image1], caption=['Cinza', 'colorida'], width=480,) 
+st.image([new_image1.convert("L"), image1], caption=['Cinza', 'colorida'], width=480,)
